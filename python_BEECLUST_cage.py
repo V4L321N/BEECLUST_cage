f63be298eed6a46d34b0f2449c5d1f6f1ab72896
V4L321N <41680849+V4L321N@users.noreply.github.com>
2019-12-19 17:01:07 +0100

    Update python_BEECLUST_cage.py

--- python_BEECLUST_cage.py
+++ python_BEECLUST_cage.py
@@ -1,34 +1,72 @@
-# this file should contain adapted "work-in-progress" SAGE code.
-# thomas uses some sort of self written pulse function that seems to work only in the SAGE Code. 
-# the internet has some very simple solutions.
-# libraries in general don't provide built-in signal functions due to one-line-implementations.
-
-# following lines are ways to create a pulse:
-# ver 1 
+import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
-t = np.linspace(0, 1, 500, endpoint=False)
-plt.plot(t, signal.square(2 * np.pi * 5 * t))
-plt.ylim(-2, 2)
 
-# ver 2 
-N = 100 # sample count
-P = 10  # period
-D = 5   # width of pulse
-sig = np.arange(N) % P < D
-plot(sig)
+tmin = 0
+tmax = 105
+t = np.linspace(tmin, tmax, tmax, endpoint=False)
 
-# ver 3 (fully configurable)
-def rect(T):
-    """create a centered rectangular pulse of width $T"""
-    return lambda t: (-T/2 <= t) & (t < T/2)
+def PULSE(start, end, height, t):
+    return np.heaviside(t - start, 0) * height - np.heaviside(t - end, 1) * height
 
-def pulse_train(t, at, shape):
-    """create a train of pulses over $t at times $at and shape $shape"""
-    return np.sum(shape(t - at[:,np.newaxis]), axis=0)
+arena_area = 30 ** 2 * np.pi                      #in cm^2
+bee_detection_area = 6                      #in cm^2
+#x1=bee_detection_area /(arena_area * 0.5)   # probability of a free bee to meet another free bee in a second
+x1 = 0.1
+x2 = 0.85                                     # bees in cage stop a moving bee more or less likely (due to cage size?)
+Cr = 6                                        # number of bees in the left cage
+Cl = 0                                        # number of bees in the right cage
+L0 = 0                                        # Initial number of bees aggregated on left side
+F0 = 24                                       # Initial number of bees running freely
+R0 = 0                                        # Initial number of bees aggregated on right side
+tmin = 0                                      # start with this value of the independent variable (t)
+tmax = 105                                    # run for this number of time steps
 
-sig = pulse_train(
-    t=np.arange(100),              # time domain
-    at=np.array([0, 10, 40, 80]),  # times of pulses
-    shape=rect(10)                 # shape of pulse
-)
+#two functions for the temperature profile
+def Tl(t):
+    return 32 + PULSE(30, tmax + 1, 4, t) #step function for left side. "height" argument is at 4 which raises temperatre to 36
+
+def Tr(t):
+    return 32 + PULSE(30, tmax + 1, 0, t) #step function for right side. "height" argument is at 0.
+
+# plt.title('temperature_BEECLUST_cage')
+# plt.plot(t, Tl(t), label = 'Temp left', linestyle = '--', color = 'blue')
+# plt.plot(t, Tr(t), label = 'Temp right', linestyle = '-.', color = 'red')
+# plt.xlabel('time [min]')
+# plt.ylabel('Temperature [°C]')
+# plt.show()
+
+#two functions that convert temperatures into waiting times
+a = -2.28
+b = 0.08
+c = 5.15
+d = 0.32
+e = 28.5
+f = 7
+
+def Wl(t):
+    return (((a + b * Tl(t)) ** c / ((a + b * Tl(t)) ** c + d ** c)) * e + f) / 15
+
+def Wr(t):
+    return (((a + b * Tr(t)) ** c / ((a + b * Tr(t)) ** c + d ** c)) * e + f) / 15
+
+# plt.plot(t, Wl(t))
+# plt.plot(t, Wr(t))
+# plt.show()
+
+Temp = np.linspace(28, 40, 100, endpoint=False)
+
+def Wartezeit(Temp):
+    return (((a + b * Temp) ** c / ((a + b * Temp) ** c + d ** c)) * e + f) / 15
+
+# plt.plot(Temp, Wartezeit(Temp))
+# plt.show()
+
+def deL(L, t):
+    return x1 * (F ** 2) + x1 * F * (L + x2 * Cl) - L / (Wl(t))
+
+def deF(F, t):
+    return L / (Wl(t)) + R / (Wr(t)) - x1 * (F ** 2) - x1 * F * (L + x2 * Cl) - x1 * (F ** 2) - x1 * F * (R + x2 * Cr)
+
+def deR(R, t):
+    return x1 * (F ** 2) + x1 * F * (R + x2 * Cr) - R / (Wr(t))
